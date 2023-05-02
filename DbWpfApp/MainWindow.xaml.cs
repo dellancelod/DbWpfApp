05638e266ca6d1ed6a44a6b5fe92588b9aee7227
dellancelod <danil.svetaylo@gmail.com>
2023-05-02 18:00:55 +0300

    Add number validation method

--- DbWpfApp/MainWindow.xaml.cs
+++ DbWpfApp/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace DbWpfApp
 {
@@ -24,5 +26,11 @@ namespace DbWpfApp
         {
             InitializeComponent();
         }
-    }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+}
 }
