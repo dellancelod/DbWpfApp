98a3fbddac0283167994512279a335b58fb1af34
dellancelod <danil.svetaylo@gmail.com>
2023-05-02 15:27:31 +0300

    Create MainWindow ViewModel

---
+++ DbWpfApp/ViewModels/MainWindowVM.cs
@@ -0,0 +1,27 @@
+﻿using DbWpfApp.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbWpfApp.ViewModels
+{
+    internal class MainWindowVM : ViewModel
+    {
+        #region Title
+        private string _Title;
+
+        /// <summary>
+        /// Заголовок вікна
+        /// </summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        } 
+        #endregion
+
+
+    }
+}
