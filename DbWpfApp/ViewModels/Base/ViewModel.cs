1366ede417394f0ae296b1e7b955d47fa0d61618
dellancelod <danil.svetaylo@gmail.com>
2023-05-02 14:53:02 +0300

    Create base ViewModel class

---
+++ DbWpfApp/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbWpfApp.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private bool _Disposed;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        // Prevent circle updates on properties to prevent stack overload
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        ~ViewModel()
+        {
+            Dispose(false);
+        }
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
+            _Disposed = true;
+            // Disposing controlled resources
+        }
+    }
+}
