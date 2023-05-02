9acbfd28234095593b9b69758a92068ff409978d
dellancelod <danil.svetaylo@gmail.com>
2023-05-02 19:50:22 +0300

    MainCommand class

---
+++ DbWpfApp/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,23 @@
+﻿using DbWpfApp.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbWpfApp.Infrastructure.Commands
+{
+    internal class LambdaCommand : CommandBase
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _canExecute = CanExecute;
+        }
+        public override bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _execute(parameter);
+    }
+}
