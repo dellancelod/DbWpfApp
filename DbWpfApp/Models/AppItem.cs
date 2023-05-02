7332389facaa5397e45bb3de53e60a631e78094c
dellancelod <danil.svetaylo@gmail.com>
2023-05-02 19:08:01 +0300

    Add App entity

---
+++ DbWpfApp/Models/AppItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DbWpfApp.Models
+{
+    internal class AppItem
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        [Display(Name = "Назва застосунку")]
+        public string AppName { get; set; }
+        [Required]
+        [Display(Name = "Ім'я користувача")]
+        public string UserName { get; set; }
+        [Display(Name = "Коментар")]
+        public string Comment { get; set; }
+    }
+}
