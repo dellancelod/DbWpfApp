e559c7c346d9de96bc34afa6b4fb6c8bb6317251
dellancelod <danil.svetaylo@gmail.com>
2023-05-02 20:08:28 +0300

    Add close application button feature

--- DbWpfApp/ViewModels/MainWindowVM.cs
+++ DbWpfApp/ViewModels/MainWindowVM.cs
@@ -1,9 +1,12 @@
-﻿using DbWpfApp.ViewModels.Base;
+﻿using DbWpfApp.Infrastructure.Commands;
+using DbWpfApp.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace DbWpfApp.ViewModels
 {
@@ -19,9 +22,25 @@ namespace DbWpfApp.ViewModels
         {
             get => _Title;
             set => Set(ref _Title, value);
-        } 
+        }
         #endregion
 
+        #region Commands
 
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommand { get; }
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+        private void OnCloseApplicationCommandExecute(object p)
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+
+        #endregion
+
+        public MainWindowVM()
+        {
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecute, CanCloseApplicationCommandExecute);
+        }
     }
 }
