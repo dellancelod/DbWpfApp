aa3df941fc108b98eeac67178b75597a68db9c21
dellancelod <danil.svetaylo@gmail.com>
2023-05-03 11:02:50 +0300

    Implement CRUD functionality

--- DbWpfApp/Data/SQLiteAppItemsService.cs
+++ DbWpfApp/Data/SQLiteAppItemsService.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DbWpfApp.Models;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SQLite;
 using System.Linq;
 using System.Text;
@@ -7,26 +10,71 @@ using System.Threading.Tasks;
 
 namespace DbWpfApp.Data
 {
-    internal class AppItemsService : IAppItemsRepository
+    internal class SQLiteAppItemsService : IAppItemsRepository
     {
         public void DeleteApp(int id)
         {
-            throw new NotImplementedException();
+            using (SQLiteConnection connection =
+                 new SQLiteConnection(SQLiteDataAccess.LoadConnectionString()))
+            {
+                connection.Open();
+                string deleteApp = $"delete from app where id = {id}";
+                SQLiteCommand command = new SQLiteCommand(deleteApp, connection);
+                command.ExecuteNonQuery();
+            }
         }
 
-        public IQueryable<App> GetApps()
+        public List<AppItem> GetApps()
         {
-            throw new NotImplementedException();
+            using (SQLiteConnection connection =
+                new SQLiteConnection(SQLiteDataAccess.LoadConnectionString()))
+            {
+                List<AppItem> returnApps = new List<AppItem>();
+                connection.Open();
+                string getApps = "select * from apps;";
+                SQLiteCommand command = new SQLiteCommand(getApps, connection);
+                SQLiteDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    AppItem app = new AppItem
+                    {
+                        Id = reader.GetInt32(0),
+                        AppName = (string)reader["app_name"],
+                        UserName = (string)reader["user_name"],
+                        Comment = (string)reader["comment"]
+                    };
+                        
+                    returnApps.Add(app);
+                }
+                return returnApps;
+            }
         }
 
-        public void SaveApp(App appItem)
+        public void SaveApp(AppItem appItem)
         {
-            throw new NotImplementedException();
+            using (SQLiteConnection connection =
+                new SQLiteConnection(SQLiteDataAccess.LoadConnectionString()))
+            {
+                connection.Open();
+                string addApp = $"insert into Apps(app_name, user_name, comment) " +
+                    $"values ('{appItem.AppName}', '{appItem.UserName}', '{appItem.Comment}')";
+                SQLiteCommand command = new SQLiteCommand(addApp, connection);
+                command.ExecuteNonQuery();
+            }
         }
 
-        public void UpdateApp(int id)
+        public void UpdateApp(int id, AppItem appItem)
         {
-            throw new NotImplementedException();
+            using (SQLiteConnection connection =
+                new SQLiteConnection(SQLiteDataAccess.LoadConnectionString()))
+            {
+                connection.Open();
+                string updateApps = $"update Apps set app_name = '{appItem.AppName}' " +
+                    $"user_name = '{appItem.UserName}' comment = '{appItem.Comment}' " +
+                    $"where id = {id}";
+                SQLiteCommand command = new SQLiteCommand(updateApps, connection);
+                command.ExecuteNonQuery();
+            }
         }
     }
 }
