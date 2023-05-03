ce747505353288fe11832285a72ed2c996ce3f66
dellancelod <danil.svetaylo@gmail.com>
2023-05-03 09:59:55 +0300

    Create Service for AppItem CRUD

---
+++ DbWpfApp/Data/AppItemsService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbWpfApp.Data
+{
+    internal class AppItemsService : IAppItemsRepository
+    {
+        public void DeleteApp(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<App> GetApps()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveApp(App appItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateApp(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DbWpfApp/Data/IAppItemsRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbWpfApp.Data
+{
+    internal interface IAppItemsRepository
+    {
+        IQueryable<App> GetApps();
+        void SaveApp(App appItem);
+        void DeleteApp(int id);
+        void UpdateApp(int id);
+    }
+}
