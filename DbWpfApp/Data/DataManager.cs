c988bc6622721c4920a708f740593fbe20f3503c
dellancelod <danil.svetaylo@gmail.com>
2023-05-03 10:12:05 +0300

    Create DataManager class

---
+++ DbWpfApp/Data/DataManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbWpfApp.Data
+{
+    internal class DataManager
+    {
+        public IAppItemsRepository AppItems { get; set; }
+        public DataManager(IAppItemsRepository appItemsRepository)
+        {
+            AppItems = appItemsRepository;
+        }
+    }
+}
