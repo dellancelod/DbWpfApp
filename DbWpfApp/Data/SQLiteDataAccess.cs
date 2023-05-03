9f55513afdb9658c9a020745d3a00d19f50244ff
dellancelod <danil.svetaylo@gmail.com>
2023-05-03 10:22:15 +0300

    Create SQLiteDataAccess class

---
+++ DbWpfApp/Data/SQLiteDataAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbWpfApp.Data
+{
+    internal class SQLiteDataAccess
+    {
+        public static string LoadConnectionString(string id)
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
